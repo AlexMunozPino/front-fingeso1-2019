ea7ac75c9fae23634551270371f949dbff91053c
Arefer <kevin.arevalo@usach.cl>
2019-07-18 00:16:25 -0400

    Vista buscar propuesta ahora incluye filtros de busqueda

--- src/pages/SearchProposal.vue
+++ src/pages/SearchProposal.vue
@@ -1,36 +1,37 @@
 <template>
   <div class="all">
     <div class="data_1">
-      <input class="prop-name" type="text" placeholder="Nombre de propuesta">
-      <input class="budget" type="number" placeholder="Presupuesto">
-      <input class="tags" type="text" placeholder="Tag1, Tag2, Tag3">
-    </div>
-    <div class="select-client">
-      <p>Seleccionar Cliente:</p>
-    </div>
-    <div class="data_2">
-      <select id="select-client">
-        <option v-for="client in this.clients" value=client>{{client.id}}</option>
+      <input v-model="search" class="prop-name" type="text" placeholder="Nombre de propuesta">
+      <input class="budget" v-model="min_budget" type="number" placeholder="Presupuesto [min]">
+      <input class="budget" v-model="max_budget" type="number" placeholder="Presupuesto [max]">
+      <!--<input class="tags" type="text" placeholder="Tag1, Tag2, Tag3">-->
+      <select v-model="selected_client_id" id="select-client" style="margin-bottom: 13px">
+        <option>Todos los clientes</option>
+        <option v-for="client in this.clients" :value="client.id">{{client.nameOfContact}}, {{client.nameOfCompany}}</option>
       </select>
     </div>
-    <div class="submit"><input id="search" type="submit" value="Buscar"></div>
     <div class="data_3">
       <table class="table">
         <tr class="table_title">
           <th>Nombre propuesta</th>
           <th>Cliente</th>
           <th>Empresa</th>
+          <th>Presupuesto</th>
           <th>Fecha de inicio</th>
-          <th>Estado</th>
+<!--          <th>Estado</th>-->
+          <th>Acción</th>
         </tr>
-        <tr v-for="proposal in this.proposals" class="table_childs" >
+        <tr v-for="proposal in this.filteredProposals" class="table_childs" >
           <td >{{proposal.name}}</td>
           <td >{{clients.filter(c => (c.id == proposal.client_id))[0].nameOfContact}}</td>
           <td >{{clients.filter(c => (c.id == proposal.client_id))[0].nameOfCompany}}</td>
+          <td >{{proposal.budget}} </td>
           <td> - </td>
-          <td> - </td>
+<!--          <td> - </td>-->
           <td> <input @click="go_to_detail(proposal.id)" type="button" value="Ver detalle"> </td>
         </tr>
+
+        <tr></tr>
       </table>
     </div>
   </div>
@@ -44,13 +45,18 @@
       name: "SearchProposal",
       data(){
           return{
-            proposals: [],
+            search: "",
+            selected_client_id: "Todos los clientes",
+            min_budget: "",
+            max_budget: "",
+            all_proposals: [],
             clients: []
           }
       },
       mounted() {
         this.retrieve_proposals();
         this.retrieve_clients();
+        document.getElementById("select-client").selectedIndex = 0;
       },
       methods: {
           go_to_detail(proposal_id){
@@ -59,7 +65,9 @@
 
           retrieve_proposals(){
             axios.get(rest_ip+"proposal/getall")
-              .then(response => {this.proposals = response.data;})
+              .then(response => {
+                this.all_proposals = response.data;
+              })
               .catch(e => {console.log(e)});
           },
 
@@ -67,8 +75,39 @@
             axios.get(rest_ip+"client/getall")
               .then(response => (this.clients = response.data))
               .catch(e => {console.log(e)});
+          },
+
+          between_budget(p){
+            if (!this.min_budget && !this.max_budget) return true;
+            else if (this.min_budget && !this.max_budget) return p.budget >= this.min_budget;
+            else if (!this.min_budget && this.max_budget) return p.budget <= this.max_budget;
+            else return p.budget >= this.min_budget && p.budget <= this.max_budget;
           }
-      }
+      },
+      computed: {
+        filteredProposals() {
+          return this.all_proposals.filter((p) => {
+            if (this.selected_client_id.localeCompare("Todos los clientes") == 0){
+              console.log(this.min_budget);
+              if (this.min_budget && this.max_budget) console.log("trick")
+              return (
+                p.name.toLowerCase().includes(this.search.toLowerCase())
+                //&& p.budget >= this.min_budget
+                //&& p.budget <= this.max_budget
+                && this.between_budget(p)
+              );
+            } else {
+              return (
+                p.name.toLowerCase().includes(this.search.toLowerCase())
+                && p.client_id.localeCompare(this.selected_client_id) == 0
+                //&& p.budget >= this.min_budget
+                //&& p.budget <= this.max_budget
+                && this.between_budget(p)
+              );
+            }
+          });
+        }
+      },
     }
 </script>
 
